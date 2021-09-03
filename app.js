703531ecf3e4a6fb324c3f136bb9f31e13f6fee1
Anita Schiller <anita.s1@gmx.de>
2021-09-03 09:20:44 +0200

    Cleaned up

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-
 fetch('https://muc-2020-w1-student-api.vercel.app/api/buddies')
   .then((res) => res.json())
-  .then((buddies) => buddies.forEach(renderTeam))
-
-
-const container = document.querySelector(".main-container");
+  .then((buddies) => buddies.forEach(renderTeam));
 
 function renderTeam(buddy) {
-  const team = document.createElement('section')
-  team.classList.add('team')
-  container.appendChild(team)
+  const container = document.querySelector('.team-container');
+  const team = document.createElement('section');
+  team.classList.add('team');
+  container.appendChild(team);
   team.innerHTML = `<div class="team__member"> ${buddy[0]} </div>
                            <img src="/Plus@2x.16876357.png" class='plus'>
-                           <div class="team__member"> ${buddy[1]} </div>`
+                           <div class="team__member"> ${buddy[1]} </div>`;
 }
-
 
 /* const wholeTeam = [
   "Andre T.",
--- journal.js
+++ journal.js
@@ -1,21 +1,24 @@
-fetch("https://muc-2020-w1-student-api.vercel.app/api/journals")
+fetch('https://muc-2020-w1-student-api.vercel.app/api/journals')
   .then((res) => res.json())
   .then((entries) => {
-    entries.forEach(renderEntries);
-    entries.forEach(createStars);
+    console.log(entries);
+    entries.forEach((entry, i) => {
+      renderEntries(entry, i);
+      createStars(entry, i);
+    });
   });
 
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 
 function renderEntries(entry, i) {
-  const card = document.createElement("div");
-  card.classList.add("journal", "sh-dark");
+  const card = document.createElement('div');
+  card.classList.add('journal', 'sh-dark');
   container.appendChild(card);
-  const id = entry.id;
   card.innerHTML = `   <p class="sebo-14-dark-up">Date</p>
         <div class="journal__div">
             <p class="reg">Rating:</p>
-            <div class="stars"></div>
+            <div class="stars-container${i}">
+            </div>
         </div>
         <div class="journal__div">
           <p class="reg">Comprehension:</p>
@@ -32,60 +35,14 @@ function renderEntries(entry, i) {
         <p class="notes">"${entry.notes}"
         </p>
     </div>`;
-
-  // paintStars(entry.rating, entry.id);
-
-  //paintStars(); */
-
-  return entry;
 }
 
-function createStars() {
-  const starsContainer = document.getElementsByClassName("stars");
-  const star1 = document.createElement("div");
-  const star2 = document.createElement("div");
-  const star3 = document.createElement("div");
-  const star4 = document.createElement("div");
-  const star5 = document.createElement("div");
-  starsContainer.appendChild(star1, star2, star3, star4, star5);
+function createStars(rating, i) {
+  const starsContainer = document.querySelector(`.stars-container${i}`);
+  let starsArray = [];
 
-  const stars = [star1, star2, star3, star4, star5];
-
-  stars.map((star) => star.classList.add("star__empty"));
-
-  //return stars;
+  for (let j = 0; j < rating; j++) {
+    const star = document.createElement('div').classList.add('star__full');
+    starsArray.push(star);
+  }
 }
-
-function paintStars(number, stars) {
-  const starsArr = Array.from(stars);
-
-  // starsArr = [star1, star2, star3, star4, star5]
-
-  const untilStar = starsArr.slice(0, number + 1);
-
-  untilStar.forEach((untilStarItem) => {
-    untilStarItem.classList.add("star__full");
-    untilStarItem.classList.remove("star__empty");
-  });
-}
-
-/* const stars = document.querySelectorAll(.star__empty)
-const starsArr = Array.from(stars) */
-
-/* function createStars(number, entry) {
-  const id = entry.id;
-  const stars = document.querySelector(id);
-  const starsArr = Array.from(stars);
-
-  // starsArr = [star1, star2, star3, star4, star5]
-
-  const untilStar = starsArr.slice(0, number + 1);
-
-  console.log(untilStar);
-
-  untilStar.forEach((untilStarItem) => {
-    untilStarItem.classList.add("star__full");
-    untilStarItem.classList.remove("star__empty");
-  });
-}
- */
