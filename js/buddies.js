f54690ec0ef9951f14993c8b54a3de982436f683
Anita Schiller <anita.s1@gmx.de>
2021-12-06 18:09:01 +0100

    Added teams and buddy fetch

--- js/buddies.js
+++ js/buddies.js
@@ -1,4 +1,4 @@
-const buddies = [
+/* const buddies = [
   ['Daniela', 'Gratian'],
   ['Janet', 'Jessica'],
   ['Joachim', 'Joel'],
@@ -7,25 +7,32 @@ const buddies = [
   ['Moritz', 'Ognjen'],
   ['Philipp', 'Thomas'],
   ['Viktoria', 'Anita'],
-];
+]; */
+
+const apiUrl = 'https://muc-student-companion-api.vercel.app/api/';
+fetch(apiUrl + 'buddies')
+  .then((data) => data.json())
+  .then((buddies) => renderBuddies(buddies));
 
 const container = document.querySelector('.team-container');
 
-buddies.forEach((buddyPair) => {
-  //Auskommentierter Teil in der buddy.html wird dynamisch für jedes buddyPair nachgebaut
-  const buddySection = document.createElement('section');
-  container.appendChild(buddySection);
-  buddySection.classList.add('team');
+function renderBuddies(buddies) {
+  buddies.forEach((buddyPair) => {
+    //Auskommentierter Teil in der buddy.html wird dynamisch für jedes buddyPair nachgebaut
+    const buddySection = document.createElement('section');
+    container.appendChild(buddySection);
+    buddySection.classList.add('team');
 
-  const plusIcon = document.createElement('img');
-  plusIcon.src = 'assets/Plus@2x.png';
-  plusIcon.classList.add('plus');
-  buddySection.appendChild(plusIcon);
+    const plusIcon = document.createElement('img');
+    plusIcon.src = 'assets/Plus@2x.png';
+    plusIcon.classList.add('plus');
+    buddySection.appendChild(plusIcon);
 
-  buddyPair.forEach((buddy) => {
-    const nameBox = document.createElement('div');
-    buddySection.appendChild(nameBox);
-    nameBox.classList.add('team__member');
-    nameBox.innerHTML = buddy;
+    buddyPair.forEach((buddy) => {
+      const nameBox = document.createElement('div');
+      buddySection.appendChild(nameBox);
+      nameBox.classList.add('team__member');
+      nameBox.innerHTML = buddy;
+    });
   });
-});
+}
